33260c9f484a0a7c89bac0146a5e3797d1d60db3
andika-eka <putuandika387@gmail.com>
2022-07-01 10:14:10 +0800

    query search and ranking

--- applicant/views.py
+++ applicant/views.py
@@ -1,22 +1,69 @@
+import re
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
+from regex import R
 from .models import Applicant
 from .forms import ApplicantForm
 from .forms import SearchForm
 from django.contrib.auth.decorators import user_passes_test
 import pdfplumber
 import pysolr 
+from django.db.models import Case, When
+
 
 # Create your views here.
+
+def give_and(text):
+    text = text.split() 
+    out = 'cv_text:"'+ text[0] + '" ' 
+    for word in text[1:]:
+        out += 'AND cv_text:"'+ word + '" ' 
+    return out
+        
+def give_or(text):
+    text = text.split() 
+    out = 'cv_text:"'+ text[0] + '" ' 
+    for word in text[1:]:
+        out += 'OR cv_text:"'+ word + '" ' 
+    return out
+        
+
 @user_passes_test(lambda u: u.is_superuser)
 def search(request):
     if request.method == 'POST':
         query = request.POST.get('search')
     solr = pysolr.Solr("http://localhost:8983/solr/cv_filter/")
-    results = solr.search('cv_text:"{0}"'.format(query))
-    ids = [result['id'] for result in results]
-    applicants = Applicant.objects.filter(id__in=ids)
+
+    query = str(query)
+    if len(query.split()) > 1:
+        solr_query = give_and(query)
+        results = solr.search(solr_query, **{
+            'rows': 500,
+            'sort': "score desc"
+        })
+        # id_and = {result['id'] for result in results}
+        ids = [result['id'] for result in results]
+        
+        # solr_query = give_or(query)
+        # results = solr.search(solr_query, **{
+        #     'rows': 500,
+        # })
+        # id_or = {result['id'] for result in results}
+
+        # ids = id_and.union(id_or)
+
+    else :
+        solr_query = 'cv_text:"'+ query + '" ' 
+
+        results = solr.search(solr_query, **{
+            'rows': 500,
+        })
+        ids = [result['id'] for result in results]
+    
+    
+    preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(ids)])
+    applicants = Applicant.objects.filter(id__in=ids).order_by(preserved)
     context = {}
     context["search_form"] = ApplicantForm()
     context["query"] = query
@@ -52,11 +99,9 @@ def indexing(request):
 
 @user_passes_test(lambda u: u.is_superuser)
 def index(request):
-    # dictionary for initial data with
-    # field names as keys
     context = {}
 
-    # add the dictionary during initialization
+
     context["applicants"] = Applicant.objects.all()
     context["search_form"] = SearchForm()
 
